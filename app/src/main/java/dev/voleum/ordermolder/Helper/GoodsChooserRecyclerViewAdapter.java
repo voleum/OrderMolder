770201787fd556952a6ba7bccd8b8915e117d2b4
voleum <voleum@github.com>
2019-10-03 20:51:51 +0300

    Have different holders for order lists

--- app/src/main/java/dev/voleum/ordermolder/Helper/GoodsChooserRecyclerViewAdapter.java
+++ app/src/main/java/dev/voleum/ordermolder/Helper/GoodsChooserRecyclerViewAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,7 +15,6 @@ import java.util.List;
 import javax.security.auth.Subject;
 
 import dev.voleum.ordermolder.Object.Good;
-import dev.voleum.ordermolder.R;
 
 public class GoodsChooserRecyclerViewAdapter extends RecyclerView.Adapter {
 
@@ -25,13 +23,11 @@ public class GoodsChooserRecyclerViewAdapter extends RecyclerView.Adapter {
     private Context mContext;
 
     public class GoodViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        public TextView goodName;
-        public EditText goodQuantity;
+        public TextView textView;
         public GoodViewHolder(View view) {
             super(view);
             view.setOnClickListener(this);
-            goodName = (TextView) view.findViewById(R.id.good_name);
-            goodQuantity = (EditText) view.findViewById(R.id.good_quantity);
+            textView = (TextView) view;
         }
 
         @Override
@@ -55,15 +51,14 @@ public class GoodsChooserRecyclerViewAdapter extends RecyclerView.Adapter {
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 //        TextView tv = new TextView(parent.getContext());
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.good_holder, parent, false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
         return new GoodViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         Good good = goods.get(position);
-        ((GoodViewHolder) holder).goodName.setText(good.toString());
-        ((GoodViewHolder) holder).goodQuantity.setText("1");
+        ((GoodViewHolder) holder).textView.setText(good.toString());
     }
 
     @Override
