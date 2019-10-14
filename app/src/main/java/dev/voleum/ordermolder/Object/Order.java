8c8246a7d4fc98e8180dc314ea14e439adaf7378
voleum <voleum@gmail.com>
2019-10-14 13:04:18 +0300

    Warehouse added

--- app/src/main/java/dev/voleum/ordermolder/Object/Order.java
+++ app/src/main/java/dev/voleum/ordermolder/Object/Order.java
@@ -2,11 +2,22 @@ package dev.voleum.ordermolder.Object;
 
 public class Order extends Document {
 
+    private String warehouseUid;
+
     public Order() {
 
     }
 
-    public Order(String uid, String date, String companyUid, String partnerUid, double sum) {
+    public Order(String uid, String date, String companyUid, String partnerUid, String warehouseUid, double sum) {
         super(uid, date, companyUid, partnerUid, sum);
+        this.warehouseUid = warehouseUid;
+    }
+
+    public String getWarehouseUid() {
+        return warehouseUid;
+    }
+
+    public void setWarehouseUid(String warehouseUid) {
+        this.warehouseUid = warehouseUid;
     }
 }
