64b464c56b1e3b1cbcdd155463ba9271f6edf0fe
voleum <voleum@github.com>
2019-11-11 23:04:21 +0300

    I don't know what's happening :(

--- app/src/main/java/dev/voleum/ordermolder/objects/Order.java
+++ app/src/main/java/dev/voleum/ordermolder/objects/Order.java
@@ -1,21 +1,36 @@
 package dev.voleum.ordermolder.objects;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import dev.voleum.ordermolder.database.DbHelper;
 
 public class Order extends Document {
 
     private String warehouseUid;
+    private List<TableGoods> tableGoods;
 
     public Order() {
 
     }
 
+    public Order(String uid) {
+        getOrderFromDb(uid);
+    }
+
     public Order(String uid, String date, String time, String companyUid, String partnerUid, String warehouseUid, double sum) {
         super(uid, date, time, companyUid, partnerUid, sum);
         this.warehouseUid = warehouseUid;
+    }
+
+    public Order(String uid, String date, String time, String companyUid, String partnerUid, String warehouseUid, double sum, List<TableGoods> tableGoods) {
+        super(uid, date, time, companyUid, partnerUid, sum);
+        this.warehouseUid = warehouseUid;
+        this.tableGoods = tableGoods;
     }
 
     @Override
@@ -33,6 +48,10 @@ public class Order extends Document {
         db.delete(DbHelper.TABLE_GOODS_TABLE,
                 whereClause,
                 whereArgs);
+        for (TableGoods tg : tableGoods
+             ) {
+            tg.save(db);
+        }
     }
 
     public String getWarehouseUid() {
@@ -41,5 +60,66 @@ public class Order extends Document {
 
     public void setWarehouseUid(String warehouseUid) {
         this.warehouseUid = warehouseUid;
+    }
+
+    public List<TableGoods> getTableGoods() {
+        return tableGoods;
+    }
+
+    private void getOrderFromDb(String uid) {
+        DbHelper dbHelper = DbHelper.getInstance();
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        String[] selectionArgs = { uid };
+        String sql = "SELECT *"
+                + " FROM " + DbHelper.TABLE_GOODS_TABLE
+                + " LEFT JOIN " + DbHelper.TABLE_GOODS
+                + " ON " + DbHelper.COLUMN_GOOD_UID + " = " + DbHelper.COLUMN_UID
+                + " WHERE " + DbHelper.COLUMN_ORDER_UID + " = ?"
+                + " ORDER BY " + DbHelper.COLUMN_POSITION;
+        Cursor c = db.rawQuery(sql, selectionArgs);
+        int positionClIndex = c.getColumnIndex(DbHelper.COLUMN_POSITION);
+        int uidClIndex = c.getColumnIndex(DbHelper.COLUMN_UID);
+        int nameClIndex = c.getColumnIndex(DbHelper.COLUMN_NAME);
+        int quantityClIndex = c.getColumnIndex(DbHelper.COLUMN_QUANTITY);
+        int priceClIndex = c.getColumnIndex(DbHelper.COLUMN_PRICE);
+        int sumClIndex = c.getColumnIndex(DbHelper.COLUMN_SUM);
+        if (c.moveToFirst()) {
+            tableGoods = new ArrayList<>();
+            do {
+                tableGoods.add(new TableGoods(c.getString(uidClIndex),
+                        c.getInt(positionClIndex),
+                        c.getString(nameClIndex),
+                        c.getDouble(quantityClIndex),
+                        c.getDouble(priceClIndex),
+                        c.getDouble(sumClIndex)));
+            } while (c.moveToNext());
+        }
+
+        String selection = DbHelper.COLUMN_UID + " = ?";
+        c = db.query(DbHelper.TABLE_ORDERS,
+                null,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                null);
+        uidClIndex = c.getColumnIndex(DbHelper.COLUMN_UID);
+        int dateClIndex = c.getColumnIndex(DbHelper.COLUMN_DATE);
+        int companyClIndex = c.getColumnIndex(DbHelper.COLUMN_COMPANY_UID);
+        int partnerClIndex = c.getColumnIndex(DbHelper.COLUMN_PARTNER_UID);
+        int warehouseClIndex = c.getColumnIndex(DbHelper.COLUMN_WAREHOUSE_UID);
+        sumClIndex = c.getColumnIndex(DbHelper.COLUMN_SUM);
+        if (c.moveToFirst()) {
+            uid = c.getString(uidClIndex);
+            dateTime = c.getString(dateClIndex);
+            companyUid = c.getString(companyClIndex);
+            partnerUid = c.getString(partnerClIndex);
+            warehouseUid = c.getString(warehouseClIndex);
+            sum = c.getDouble(sumClIndex);
+        }
+
+        c.close();
+        db.close();
     }
 }
