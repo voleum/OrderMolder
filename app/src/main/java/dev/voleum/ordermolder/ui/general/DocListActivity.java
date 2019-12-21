8c7fad73456a99a69d788df9e1548852d0458e0a
voleum <voleum@gmail.com>
2019-12-21 22:49:28 +0300

    ViewModel in Doc list

--- app/src/main/java/dev/voleum/ordermolder/ui/general/DocListActivity.java
+++ app/src/main/java/dev/voleum/ordermolder/ui/general/DocListActivity.java
@@ -13,7 +13,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.util.ArrayList;
 import java.util.Objects;
 
 import dev.voleum.ordermolder.R;
@@ -28,9 +27,10 @@ import dev.voleum.ordermolder.viewmodels.DocListViewModel;
 public class DocListActivity extends AppCompatActivity {
 
     public final static String IS_CREATING = "is_creating";
-    public final static String DOC_ACTIVITY = "doc_activity";
     public final static String DOC = "doc";
+    public final static String DOC_ACTIVITY = "doc_activity";
     public final static String DOC_TYPE = "doc_type";
+    public final static String POSITION = "position";
 
     public final static int REQUEST_CODE = 0;
     public final static int RESULT_SAVED = 2;
@@ -39,8 +39,6 @@ public class DocListActivity extends AppCompatActivity {
     private DocListViewModel docListViewModel;
 
     private RecyclerView recyclerDocs;
-    private ArrayList<Document> arrayDocs;
-    private DocListRecyclerViewAdapter adapter;
     private DocumentTypes docType;
 
     FloatingActionButton fab;
@@ -57,12 +55,12 @@ public class DocListActivity extends AppCompatActivity {
         binding.setViewModel(docListViewModel);
         binding.executePendingBindings();
 
-        recyclerDocs = binding.getRoot().findViewById(R.id.recycler_objs);
+        recyclerDocs = binding.getRoot().findViewById(R.id.recycler_docs);
         recyclerDocs.setHasFixedSize(true);
         recyclerDocs.setLayoutManager(new LinearLayoutManager(this));
 
         DocListRecyclerViewAdapter.OnEntryCLickListener onEntryCLickListener = (v, position) -> {
-            Document clickedDoc = arrayDocs.get(position);
+            Document clickedDoc = binding.getViewModel().getDocs().get(position);
             Intent intentOut;
             switch (docType) {
                 case ORDER:
@@ -76,8 +74,11 @@ public class DocListActivity extends AppCompatActivity {
             }
             intentOut.putExtra(IS_CREATING, false);
             intentOut.putExtra(DOC, clickedDoc.getUid());
+            intentOut.putExtra(POSITION, position);
             startActivityForResult(intentOut, REQUEST_CODE);
         };
+
+        binding.getViewModel().getAdapter().setOnEntryCLickListener(onEntryCLickListener);
 
         View.OnClickListener fabClickListener = v -> {
             Intent intentOut;
@@ -92,21 +93,12 @@ public class DocListActivity extends AppCompatActivity {
                     intentOut = null;
             }
             intentOut.putExtra(IS_CREATING, true);
+            intentOut.putExtra(POSITION, docListViewModel.getDocs().size());
             startActivityForResult(intentOut, REQUEST_CODE);
         };
 
-//        docType = (DocumentTypes) getIntent().getSerializableExtra(DOC_TYPE);
-//        setContentView(R.layout.activity_doc_list);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-//        recyclerDocs = findViewById(R.id.recycler_objs);
-//        recyclerDocs.setHasFixedSize(true);
-//        recyclerDocs.setLayoutManager(new LinearLayoutManager(this));
-//        fillDocList();
-//        adapter = new DocListRecyclerViewAdapter(arrayDocs);
-//        adapter.setOnEntryCLickListener(onEntryCLickListener);
-//        recyclerDocs.setAdapter(adapter);
 
         fab = findViewById(R.id.doc_list_fab);
         fab.setOnClickListener(fabClickListener);
@@ -128,25 +120,15 @@ public class DocListActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        int position;
+        Document doc = null;
+        if (resultCode != 0) doc = (Document) data.getSerializableExtra(DOC);
         switch (resultCode) {
-//            case RESULT_SAVED:
-//                try {
-//                    position = arrayDocs.indexOf(data.getSerializableExtra(DOC));
-//                    adapter.notifyItemChanged(position);
-//                } catch (NullPointerException e) {
-//                    e.printStackTrace();
-//                }
-//                break;
-//            case RESULT_CREATED:
-//                position = arrayDocs.size();
-//                try {
-//                    arrayDocs.add(position, (Document) data.getSerializableExtra(DOC));
-//                    adapter.notifyItemInserted(position + 1);
-//                } catch (NullPointerException e) {
-//                    e.printStackTrace();
-//                }
-//                break;
+            case RESULT_SAVED:
+                docListViewModel.editDoc(doc, data.getIntExtra(POSITION, -1));
+                break;
+            case RESULT_CREATED:
+                docListViewModel.addDoc(doc);
+                break;
         }
     }
 
@@ -155,71 +137,4 @@ public class DocListActivity extends AppCompatActivity {
         onBackPressed();
         return true;
     }
-
-//    private void fillDocList() {
-//
-//        arrayDocs = new ArrayList<>();
-//
-//        // TODO: Async
-//        DbHelper dbHelper = DbHelper.getInstance();
-//        SQLiteDatabase db = dbHelper.getReadableDatabase();
-//
-//        String table;
-//
-//        switch (docType) {
-//            case ORDER:
-//                table = DbHelper.TABLE_ORDERS;
-//                break;
-//            case CASH_RECEIPT:
-//                table = DbHelper.TABLE_CASH_RECEIPTS;
-//                break;
-//            default:
-//                table = "";
-//        }
-//
-//        Cursor c = db.query(table,
-//                null,
-//                null,
-//                null,
-//                null,
-//                null,
-//                null);
-//
-//        if (c.moveToFirst()) {
-//            int uidIndex = c.getColumnIndex((DbHelper.COLUMN_UID));
-//            int dateIndex = c.getColumnIndex((DbHelper.COLUMN_DATE));
-//            int companyIndex = c.getColumnIndex((DbHelper.COLUMN_COMPANY_UID));
-//            int partnerIndex = c.getColumnIndex((DbHelper.COLUMN_PARTNER_UID));
-//            int warehouseIndex = -1;
-//            if (docType == ORDER) {
-//                warehouseIndex = c.getColumnIndex((DbHelper.COLUMN_WAREHOUSE_UID));
-//            }
-//            int sumIndex = c.getColumnIndex((DbHelper.COLUMN_SUM));
-//            switch (docType) {
-//                case ORDER:
-//                    do {
-//                        arrayDocs.add(new Order(c.getString(uidIndex),
-//                                c.getString(dateIndex),
-//                                c.getString(companyIndex),
-//                                c.getString(partnerIndex),
-//                                c.getString(warehouseIndex),
-//                                c.getDouble(sumIndex)));
-//                    } while (c.moveToNext());
-//                    break;
-//                case CASH_RECEIPT:
-//                    do {
-//                        arrayDocs.add(new CashReceipt(c.getString(uidIndex),
-//                                c.getString(dateIndex),
-//                                c.getString(companyIndex),
-//                                c.getString(partnerIndex),
-//                                c.getDouble(sumIndex)));
-//                    } while (c.moveToNext());
-//                    break;
-//            }
-//
-//        }
-//
-//        c.close();
-//        db.close();
-//    }
 }
--- app/src/main/java/dev/voleum/ordermolder/viewmodels/DocListViewModel.java
+++ app/src/main/java/dev/voleum/ordermolder/viewmodels/DocListViewModel.java
@@ -17,6 +17,9 @@ import dev.voleum.ordermolder.enums.DocumentTypes;
 import dev.voleum.ordermolder.objects.CashReceipt;
 import dev.voleum.ordermolder.objects.Document;
 import dev.voleum.ordermolder.objects.Order;
+import io.reactivex.Completable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 import static dev.voleum.ordermolder.enums.DocumentTypes.ORDER;
 
@@ -28,8 +31,10 @@ public class DocListViewModel extends BaseObservable {
 
     public DocListViewModel(DocumentTypes docType) {
         this.docType = docType;
-        initDocList();
-        this.adapter = new DocListRecyclerViewAdapter(docs);
+        initDocList()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe();
     }
 
     @Bindable
@@ -54,70 +59,83 @@ public class DocListViewModel extends BaseObservable {
         }
     }
 
-    private void initDocList() {
+    public void addDoc(Document doc) {
+        docs.add(doc);
+        notifyChange();
+    }
 
-        docs = new ArrayList<>();
+    public void editDoc(Document doc, int position) {
+        docs.set(position, doc);
+        notifyChange();
+    }
 
-        // TODO: Async
-        DbHelper dbHelper = DbHelper.getInstance();
-        SQLiteDatabase db = dbHelper.getReadableDatabase();
+    private Completable initDocList() {
 
-        String table;
+        return Completable.create(subscriber -> {
 
-        switch (docType) {
-            case ORDER:
-                table = DbHelper.TABLE_ORDERS;
-                break;
-            case CASH_RECEIPT:
-                table = DbHelper.TABLE_CASH_RECEIPTS;
-                break;
-            default:
-                table = "";
-        }
+            docs = new ArrayList<>();
 
-        Cursor c = db.query(table,
-                null,
-                null,
-                null,
-                null,
-                null,
-                null);
+            DbHelper dbHelper = DbHelper.getInstance();
+            SQLiteDatabase db = dbHelper.getReadableDatabase();
 
-        if (c.moveToFirst()) {
-            int uidIndex = c.getColumnIndex((DbHelper.COLUMN_UID));
-            int dateIndex = c.getColumnIndex((DbHelper.COLUMN_DATE));
-            int companyIndex = c.getColumnIndex((DbHelper.COLUMN_COMPANY_UID));
-            int partnerIndex = c.getColumnIndex((DbHelper.COLUMN_PARTNER_UID));
-            int warehouseIndex = -1;
-            if (docType == ORDER) {
-                warehouseIndex = c.getColumnIndex((DbHelper.COLUMN_WAREHOUSE_UID));
-            }
-            int sumIndex = c.getColumnIndex((DbHelper.COLUMN_SUM));
+            String table;
+
             switch (docType) {
                 case ORDER:
-                    do {
-                        docs.add(new Order(c.getString(uidIndex),
-                                c.getString(dateIndex),
-                                c.getString(companyIndex),
-                                c.getString(partnerIndex),
-                                c.getString(warehouseIndex),
-                                c.getDouble(sumIndex)));
-                    } while (c.moveToNext());
+                    table = DbHelper.TABLE_ORDERS;
                     break;
                 case CASH_RECEIPT:
-                    do {
-                        docs.add(new CashReceipt(c.getString(uidIndex),
-                                c.getString(dateIndex),
-                                c.getString(companyIndex),
-                                c.getString(partnerIndex),
-                                c.getDouble(sumIndex)));
-                    } while (c.moveToNext());
+                    table = DbHelper.TABLE_CASH_RECEIPTS;
                     break;
+                default:
+                    return;
             }
 
-        }
+            String orderBy = DbHelper.COLUMN_DATE;
 
-        c.close();
-        db.close();
+            Cursor c = db.query(table,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    orderBy);
+
+            if (c.moveToFirst()) {
+                int uidIndex = c.getColumnIndex((DbHelper.COLUMN_UID));
+                int dateIndex = c.getColumnIndex((DbHelper.COLUMN_DATE));
+                int companyIndex = c.getColumnIndex((DbHelper.COLUMN_COMPANY_UID));
+                int partnerIndex = c.getColumnIndex((DbHelper.COLUMN_PARTNER_UID));
+                int warehouseIndex = -1;
+                if (docType == ORDER) {
+                    warehouseIndex = c.getColumnIndex((DbHelper.COLUMN_WAREHOUSE_UID));
+                }
+                int sumIndex = c.getColumnIndex((DbHelper.COLUMN_SUM));
+                switch (docType) {
+                    case ORDER:
+                        do {
+                            docs.add(new Order(c.getString(uidIndex),
+                                    c.getString(dateIndex),
+                                    c.getString(companyIndex),
+                                    c.getString(partnerIndex),
+                                    c.getString(warehouseIndex),
+                                    c.getDouble(sumIndex)));
+                        } while (c.moveToNext());
+                        break;
+                    case CASH_RECEIPT:
+                        do {
+                            docs.add(new CashReceipt(c.getString(uidIndex),
+                                    c.getString(dateIndex),
+                                    c.getString(companyIndex),
+                                    c.getString(partnerIndex),
+                                    c.getDouble(sumIndex)));
+                        } while (c.moveToNext());
+                        break;
+                }
+            }
+            c.close();
+            db.close();
+            adapter = new DocListRecyclerViewAdapter(docs);
+        });
     }
 }
