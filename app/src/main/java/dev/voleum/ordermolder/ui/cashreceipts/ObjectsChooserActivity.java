d083cf4b83283e2ca772e5bb339abd79fe7bb812
voleum <voleum@gmail.com>
2020-01-03 21:06:07 +0300

    Fixed Rx

--- app/src/main/java/dev/voleum/ordermolder/ui/cashreceipts/ObjectsChooserActivity.java
+++ app/src/main/java/dev/voleum/ordermolder/ui/cashreceipts/ObjectsChooserActivity.java
@@ -17,11 +17,16 @@ import dev.voleum.ordermolder.database.DbHelper;
 import dev.voleum.ordermolder.databinding.ActivityObjectsChooserBinding;
 import dev.voleum.ordermolder.objects.Order;
 import dev.voleum.ordermolder.viewmodels.ObjectsChooserViewModel;
+import io.reactivex.Completable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 public class ObjectsChooserActivity extends AppCompatActivity {
 
     public static final String OBJECT = "object";
     public static final String SUM = "sum";
+
+    private ActivityObjectsChooserBinding binding;
 
     private ObjectsChooserViewModel objectsChooserViewModel;
 
@@ -31,37 +36,46 @@ public class ObjectsChooserActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setTitle(R.string.title_activity_orders);
-
-        objectsChooserViewModel
-                = new ObjectsChooserViewModel(getIntent().getStringExtra(DbHelper.COLUMN_COMPANY_UID),
-                    getIntent().getStringExtra(DbHelper.COLUMN_PARTNER_UID));
-
-        ActivityObjectsChooserBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_objects_chooser);
-        binding.setViewModel(objectsChooserViewModel);
-        binding.executePendingBindings();
-
-        recyclerObjects = binding.getRoot().findViewById(R.id.recycler_objects_chooser);
-        recyclerObjects.setHasFixedSize(true);
-        recyclerObjects.setLayoutManager(new LinearLayoutManager(this));
-
-        binding.getViewModel().getAdapter().setOnEntryClickListener((v, position) -> {
-            HashMap<String, Object> chosen = binding.getViewModel().getObjects().get(position);
-            double sum = (double) chosen.get(ObjectsChooserActivity.SUM);
-            setResult(RESULT_OK, new Intent()
-                    .putExtra(OBJECT, (Order) chosen.get(OBJECT))
-                    .putExtra(SUM, sum));
-            finish();
-        });
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_objects_chooser);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null) getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        initData()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe();
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    private Completable initData() {
+        return Completable.create(subscriber -> {
+            objectsChooserViewModel
+                    = new ObjectsChooserViewModel(getIntent().getStringExtra(DbHelper.COLUMN_COMPANY_UID),
+                    getIntent().getStringExtra(DbHelper.COLUMN_PARTNER_UID));
+
+            binding.setViewModel(objectsChooserViewModel);
+            binding.executePendingBindings();
+
+            recyclerObjects = binding.getRoot().findViewById(R.id.recycler_objects_chooser);
+            recyclerObjects.setHasFixedSize(true);
+            recyclerObjects.setLayoutManager(new LinearLayoutManager(this));
+
+            binding.getViewModel().getAdapter().setOnEntryClickListener((v, position) -> {
+                HashMap<String, Object> chosen = binding.getViewModel().getObjects().get(position);
+                double sum = (double) chosen.get(ObjectsChooserActivity.SUM);
+                setResult(RESULT_OK, new Intent()
+                        .putExtra(OBJECT, (Order) chosen.get(OBJECT))
+                        .putExtra(SUM, sum));
+                finish();
+            });
+            setTitle(R.string.title_activity_orders);
+        });
     }
 }
