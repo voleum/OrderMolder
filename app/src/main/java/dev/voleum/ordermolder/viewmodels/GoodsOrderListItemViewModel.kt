997e33e2f056d989e551e7f8a25032012df307e3
voleum <voleum@github.com>
2020-03-23 13:20:12 +0300

    UI fixes

---
+++ app/src/main/java/dev/voleum/ordermolder/viewmodels/GoodsOrderListItemViewModel.kt
@@ -0,0 +1,38 @@
+package dev.voleum.ordermolder.viewmodels
+
+import android.view.View
+import androidx.databinding.Bindable
+import dev.voleum.ordermolder.R
+import dev.voleum.ordermolder.models.TableGoods
+
+class GoodsOrderListItemViewModel(table: TableGoods, viewModel: OrderViewModel) : AbstractDocListItemViewModel<TableGoods, OrderViewModel>(table, viewModel), View.OnClickListener {
+
+    var quantity: String = ""
+        @Bindable set(value) {
+            val q = value.toDouble()
+            table.quantity = q
+            table.countSum()
+            viewModel.countSum()
+            field = q.toString()
+        }
+        @Bindable get() = df.format(table.quantity)
+
+    var price: String = ""
+        @Bindable set(value) {
+            val p = value.toDouble()
+            table.price = p
+            table.countSum()
+            viewModel.countSum()
+            field = p.toString()
+        }
+        @Bindable get() = df.format(table.price)
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.good_plus -> table.increaseQuantity()
+            R.id.good_minus -> table.decreaseQuantity()
+        }
+        notifyChange()
+        viewModel.countSum()
+    }
+}
